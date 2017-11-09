4093cbb8d4a5084f22650cced71981e73b241f27
kirillafendi83 <kyrylo.afendi@nure.ua>
2017-11-10 01:26:41 +0200

    New method GetUnknownWords

--- src/processing/ProcessText.js
+++ src/processing/ProcessText.js
@@ -8,9 +8,30 @@ const needModules = [
     new DigitCleaner()
 ];
 
-export function ProcessTextAllModules(text){
-    for(let i = 0; i < needModules.length; i++){
-        text = needModules[i].startProcessing(text);
+export default {
+
+    ProcessTextAllModules: function (text) {
+        for (let i = 0; i < needModules.length; i++) {
+            text = needModules[i].startProcessing(text);
+        }
+        return text;
+    },
+    GetUnknownWords: function (allWords, knownWords) {
+        let result = [];
+        let knowWords = [];
+        for(let i = 0; i < allWords.length; i++) {
+            let isKnown = false;
+            for(let t = 0; t < knownWords.length; t++) {
+                if(allWords[i] === knownWords[t].Word_value){
+                    knowWords.push(knownWords[t]);
+                    isKnown = true;
+                    break;
+                }
+            }
+            if(!isKnown){
+                result.push(allWords[i]);
+            }
+        }
+        return result;
     }
-    return text;
 }
